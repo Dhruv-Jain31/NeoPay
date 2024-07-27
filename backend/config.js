3d6d67a02fd0108508489e58406ecf25add6cb85
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-07-27 20:20:39 +0530

    added routes and jwt secret

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = "djsecret";
+
+module.exports = JWT_SECRET
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
-const express = require('express')
+const express = require('express');
+const cors = require("cors");
+const mainRouter = require("./routes/index")
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/v1", mainRouter); // routes all requests coming to api/ to mainRouter
+
+app.listen(5600, function(){
+    console.log(`Neopay is running on localhost:5600`);
+})
